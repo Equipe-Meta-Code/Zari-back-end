cafe464252934cb3e8aaf0cbbe3212561604b8e4
Dianne Faria de Brito <diannefaria09@gmail.com>
2023-09-22 11:26:25 -0300

    Sprint 1 - Back-end da tela perguntas e respostas

---
+++ Engenharia/lib/src/main/java/_Engenharia/Principal.java
@@ -0,0 +1,23 @@
+ 
+package _Engenharia;
+
+import GUI.PerguntaResposta;
+
+/**
+ *
+ * @author diann
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        new PerguntaResposta();
+        
+//"Where does Charlie lives?"
+        
+    }
+    
+}
