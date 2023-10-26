f8b02df590d8c908bc1bf0d6ff6656aa57cda974
Maria Luiza <mluizaguedessilva@gmail.com>
2023-10-26 11:08:07 -0300

    Adição de data e hora no modelo Histórico

--- Engenharia/src/main/java/model/Historico.java
+++ Engenharia/src/main/java/model/Historico.java
@@ -3,12 +3,22 @@ package model;
 public class Historico {
     private String emailHistorico;
     private String documento;
+    private String dataEHora;
 
 
-    public Historico(String emailHistorico, String documento) {
+    public Historico(String emailHistorico, String documento, String dataEHora) {
         this.emailHistorico = emailHistorico;
         this.documento = documento;
+        this.dataEHora = dataEHora;
     }
+
+    public String getDataEHora() {
+		return dataEHora;
+	}
+
+	public void setDataEHora(String dataEHora) {
+		this.dataEHora = dataEHora;
+	}
 
     public String getEmailHistorico() {
         return emailHistorico;
